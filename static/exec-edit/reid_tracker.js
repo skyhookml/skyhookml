df556a0748f916605a18477d9a1fcacd67723da9
Favyen Bastani <fbastani@perennate.com>
2021-04-13 22:02:04 -0400

    Add documentation for re-id tracker parameters.

--- static/exec-edit/reid_tracker.js
+++ static/exec-edit/reid_tracker.js
@@ -42,27 +42,42 @@ export default {
 <div class="small-container m-2">
 	<template v-if="node != null">
 		<div class="form-group row">
-			<label class="col-sm-2 col-form-label">Velocity Steps</label>
-			<div class="col-sm-10">
+			<label class="col-sm-4 col-form-label">Velocity Steps</label>
+			<div class="col-sm-8">
 				<input v-model="velocitySteps" type="text" class="form-control">
+				<small class="form-text text-muted">
+					The number of frames to use when estimating an object's velocity.
+					For example, 5 means an object's velocity will be estimated as its displacement over 5 frames divided by 5.
+					Velocity is used to estimate an object's likely current position based on its previous position.
+				</small>
 			</div>
 		</div>
 		<div class="form-group row">
-			<label class="col-sm-2 col-form-label">Minimum IOU</label>
-			<div class="col-sm-10">
+			<label class="col-sm-4 col-form-label">Minimum IOU</label>
+			<div class="col-sm-8">
 				<input v-model="minIOU" type="text" class="form-control">
+				<small class="form-text text-muted">
+					IOU is the intersection-over-union area, and measures overlap between bounding boxes.
+					A bounding box in a later frame must overlap with an object's estimated position by at least this much IOU to be associated with the object.
+				</small>
 			</div>
 		</div>
 		<div class="form-group row">
-			<label class="col-sm-2 col-form-label">Maximum Age</label>
-			<div class="col-sm-10">
+			<label class="col-sm-4 col-form-label">Maximum Age</label>
+			<div class="col-sm-8">
 				<input v-model="maxAge" type="text" class="form-control">
+				<small class="form-text text-muted">
+					If we have not seen an object for this many frames, we assume it is gone and remove it.
+				</small>
 			</div>
 		</div>
 		<div class="form-group row">
-			<label class="col-sm-2 col-form-label">Re-identification weight</label>
-			<div class="col-sm-10">
+			<label class="col-sm-4 col-form-label">Re-identification Weight</label>
+			<div class="col-sm-8">
 				<input v-model="weight" type="text" class="form-control">
+				<small class="form-text text-muted">
+					Weight of re-identification model relative to the heuristic motion estimator.
+				</small>
 			</div>
 		</div>
 		<button v-on:click="save" type="button" class="btn btn-primary">Save</button>
