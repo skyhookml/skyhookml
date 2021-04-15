18308e170b45c81c4707c5081d2e50f0dfe2a66d
Favyen Bastani <fbastani@perennate.com>
2021-04-15 18:18:39 -0400

    Clarify that Pipeline page is called Pipeline.

--- static/pipeline.js
+++ static/pipeline.js
@@ -15,7 +15,10 @@ export default {
 	template: `
 <div class="flex-container">
 	<div class="my-2">
-		<div class="btn-group">
+		<div class="float-start">
+			<h3>Pipeline</h3>
+		</div>
+		<div class="btn-group float-end">
 			<button
 				class="btn btn-outline-secondary shadow-none"
 				:class="{active: mode == 'table'}"
